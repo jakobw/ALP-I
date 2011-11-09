fd1f40d3abd943897933f036acd2c6578f7c9c4a
Jakob Warkotsch <jakob@lempa7.com>
2011-11-09 13:02:01 +0100

    24.

--- ub4.hs
+++ ub4.hs
@@ -33,3 +33,29 @@ zinsen kapital zinsfuß = kapital * zinsfuß * 0.01
 blah :: [Double] -> [Double]
 blah = map (`zinsen` 2.25)
 -- Die Funktion berechnet die Zinsen zu einer Liste aus Kapitalangaben mit dem vorgegebenen Zinsfuß 2.25. (2.25 gibt den Wert für zinsfuß vor, weil zinsen als Infix-Funktion geschrieben ist.)
+
+{-
+  24)
+  (a) (10 Punkte) Bei der Lauflängenkodierung wird eine Kette "aaaabbaaa" mit vielen wiederholten Zeichen komprimiert, indem man die Länge jedes Laufes von gleichen Zeichen nimmt: [(4,'a'), (2,'b'), (3,'a')]. Schreiben Sie eine Funktion, die diese Kodierung berechnet, und auch die Umkehrfunktion für die Dekodierung.
+  (b) (10 Punkte) Unter der Annahme, dass die Eingabekette keine Ziffern enthält, kann man die komprimierte Fassung kompakter als Kette "4a2b3a" darstellen. Erweitern Sie die vorige Aufgabe auf diese Darstellung
+-}
+
+-- a)
+runLengthDecode :: [(Int, Char)] -> String
+runLengthDecode list = foldr (\(n, chr) text -> replicate n chr ++ text) "" list
+
+runLengthEncode :: String -> [(Int, Char)]
+runLengthEncode "" = []
+runLengthEncode str =
+  let count = countFirst str
+  in (count, head str): runLengthEncode (drop count str)
+  where
+    countFirst str = length (takeWhile (== head str) str)
+
+-- b)
+runLengthDecode' :: String -> String
+runLengthDecode' [] = ""
+runLengthDecode' (n:chr:xs) = replicate (read [n]) chr ++ runLengthDecode' xs
+
+runLengthEncode' :: String -> String
+runLengthEncode' str = foldr (\(n, chr) encStr -> show n ++ [chr] ++ encStr) "" (runLengthEncode str)
