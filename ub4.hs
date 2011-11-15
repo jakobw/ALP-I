6655426fe20ffcdb40e93dd76b693aee6e6a06d0
Jakob Warkotsch <jakob@lempa7.com>
2011-11-15 11:21:37 +0100

    Add fancy haskell syntax.

--- ub4.hs
+++ ub4.hs
@@ -3,8 +3,10 @@
   Drücken Sie die Funktion length mit Hilfe von sum und map und geeigneten selbstdefinierten Funktionen aus. (Eine Zeile sollte für die Definition reichen.)
 -}
 
-length' :: [a] -> Int
+length', length'' :: [a] -> Int
 length' a = sum (map (\_ -> 1) a)
+
+length'' = sum.map (const 1)
 
 {-
   20)
